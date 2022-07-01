500b92b7b169563fcfd6295e7100937e743a0461
hubertwisniewski0 <100858757+hubertwisniewski0@users.noreply.github.com>
2022-07-01 18:47:21 +0200

    Translate code (#1)
    * Translate game core code
    
    * Translate interface
    
    * Translate window
    
    * Translate timer
    
    * Translate board
    
    * Translate text class
    
    * Translate legend
    
    * Translate texts
    
    * Translate main
    
    * Move all classes to separate, properly named files

--- src/main.cpp
+++ src/main.cpp
@@ -4,33 +4,34 @@
 #include<SDL.h>
 #include<SDL_ttf.h>
 #define XOFFSET 40
-void wiadomosc_o_bledzie(std::string tresc)
+void errorMessage(std::string message)
 {
     if(SDL_WasInit(SDL_INIT_VIDEO))
-        SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR,"Błąd",tresc.c_str(),NULL);
+        SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Error", message.c_str(), NULL);
     else
-        std::cerr << tresc << "\n";
+        std::cerr << message << "\n";
 }
-void wiadomosc_o_wygranej(std::string tresc, SDL_Window* okno)
+void victoryMessage(std::string message, SDL_Window* window)
 {
-    SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_INFORMATION,"Statki",tresc.c_str(),okno);
+    SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_INFORMATION, "Victory", message.c_str(), window);
 }
-#include"statki.hpp"
-#include"tekst.hpp"
-#include"legenda.hpp"
-#include"napisy.hpp"
-#include"plansza.hpp"
-#include"zegar.hpp"
-#include"okno.hpp"
-#include"interfejs.hpp"
+#include"Game.hpp"
+#include"Enemy.hpp"
+#include"Text.hpp"
+#include"Legend.hpp"
+#include"Texts.hpp"
+#include"Board.hpp"
+#include"Timer.hpp"
+#include"Window.hpp"
+#include"GUI.hpp"
 int main(int argc, char* argv[])
 {
     srand(time(NULL));
-    CGra Gra;
-    CWrog Wrog(&Gra);
-    CInterfejs Interfejs(&Gra,&Wrog);
-    if(!Interfejs.ok())
+    Game game;
+    Enemy enemy(&game);
+    GUI gui(&game, &enemy);
+    if(!gui.ok())
         return 1;
-    Interfejs.start();
+    gui.start();
     return 0;
 }
